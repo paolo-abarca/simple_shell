194a875f28ab14278c77e62173b84a2841028345
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-04-11 08:31:29 -0500

    updating function that prints error messages

--- error_print.c
+++ error_print.c
@@ -1,47 +1,85 @@
 #include "main.h"
 
 /**
- * error_printing - print error messages
- *
- * @argv: is the name of the program
- * @num: number of order
- * @array: the original command
+ * error_printing - Entry point
+ *@av: command
+ *@count: tokens
+ *@command: char string
  * Return: Always 0 (Success)
  */
-
-void error_printing(char *argv, char num, char *array)
+void error_printing(char *av, int count, char *command)
 {
-	print_string(argv, 1);
+	print_string(av, 1);
 	print_string(": ", 1);
-	_write(num);
+	print_number(count);
 	print_string(": ", 1);
-	print_string(array, 1);
+	print_string(command, 1);
 }
 /**
- * print_string - print the error_printing strings
- *
- * @string: char string
- * @new_line: is for a line break
+ * exec_error - Entry point
+ *@av: command[0]
+ *@count: count tokens
+ *@tmp_command: command
  * Return: void
  */
-void print_string(char *string, int new_line)
+void exec_error(char *av, int count, char *tmp_command)
+{
+	error_printing(av, count, tmp_command);
+	print_string(": ", 1);
+	perror("");
+	exit(1);
+}
+/**
+ * print_string - Entry point
+ *@str: char string
+ *@new_line: string
+ * Return: void
+ */
+void print_string(char *str, int new_line)
 {
 	int i;
 
-	if (string == NULL)
-		string = "(null)";
-	for (i = 0; string[i] != '\0'; i++)
-		write(STDOUT_FILENO, &string[i], 1);
+	if (str == NULL)
+		str = "(null)";
+	for (i = 0; str[i] != '\0'; i++)
+		write(STDOUT_FILENO, &str[i], 1);
 	if (new_line == 0)
 		write(STDOUT_FILENO, "\n", 1);
 }
 /**
- * _write - print the order number
- *
- * @num: is the order number
- * Return: Always 0
+ * print_number - Entry point
+ *@n: int
+ * Return: length
  */
-int _write(char num)
+int print_number(int n)
 {
-	return (write(1, &num, 1));
+	int div;
+	int len;
+	unsigned int num;
+
+	div = 1;
+	len = 0;
+
+	num = n;
+
+	for (; num / div > 9; )
+		div *= 10;
+
+	for (; div != 0; )
+	{
+		len += _write('0' + num / div);
+		num %= div;
+		div /= 10;
+	}
+
+	return (len);
+}
+/**
+ * _write - Entry point
+ *@c: character
+ * Return: Always 0 (Success)
+ */
+int _write(char c)
+{
+	return (write(1, &c, 1));
 }
