4dfae1cbc5180043ce9a176cbc474082d9043f84
gerardomp18 <gerardomarinparra18@gmail.com>
2022-04-11 06:45:58 -0500

    Function to list the environment

---
+++ get_environ.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _getenv - get the folders from the PATH variable
+ *
+ * @value: is the PATH
+ * Return: all the folders in the PATH
+ */
+
+char *_getenv(const char *value)
+{
+	int len = _strlen((char *)value);
+	char **env = environ;
+
+	while (*env != NULL)
+	{
+		if (_strncmp(*env, (char *)value, len) == 0 && env[0][len] == '=')
+			return (*env + len + 1);
+		env++;
+	}
+	return (NULL);
+}
