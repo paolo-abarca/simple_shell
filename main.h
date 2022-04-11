d140f96f6ace071bf2aca4fda7611b861086477a
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-04-11 15:39:47 -0500

    adding new version of functions to shell

--- main.h
+++ main.h
@@ -8,33 +8,26 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
-#include <sys/types.h>
 #include <time.h>
-#include <libgen.h>
 
 extern char **environ;
 
 /* functions */
 int check_string(char *string);
 char **tokenizer(char *string);
+int _strlen(char *string);
 unsigned int word_counter(char *string);
 void array_free(char **array);
+char *_strncpy(char *dest, char *src, int n);
 int commands_functions(char *string, char **array, int *retVal);
+int _strncmp(char *s1, char *s2, int n);
 void parent_free(char *string, char **array);
 void my_env(char **environ);
 char *findpath(char *array, int *retVal);
-char *_strcat(char *s1, char *s2);
-
-
 char *_getenv(const char *name);
-int _strlen(char *s);
-int _strncmp(char *s1, char *s2, int n);
-char *str_concat(char *s1, char *s2);
-char *_strdup(char *str);
-char *_strncpy(char *dest, char *src, int n);
+char *_strcat(char *s1, char *s2);
 void error_printing(char *av, int count, char *command);
-void exec_error(char *av, int count, char *tmp_command);
-void print_string(char *str, int new_line);
+void print_string(char *string, int new_line);
 int print_number(int n);
 int _write(char c);
 
