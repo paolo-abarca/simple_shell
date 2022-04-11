0eb1e99653739acb89871fb5bba4c4990e0ecc27
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-04-11 08:52:00 -0500

    fixing main function of simple shell

--- main.h
+++ main.h
@@ -1,8 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#define _GNU_SOURCE
-
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -15,29 +13,21 @@
 #include <libgen.h>
 
 extern char **environ;
+
+/* functions */
 char *_getenv(const char *name);
 char *findpath(char *command, int *retVal);
-
-/*str_tools*/
 int _strlen(char *s);
 int _strncmp(char *s1, char *s2, int n);
 char *str_concat(char *s1, char *s2);
 char *_strdup(char *str);
 char *_strncpy(char *dest, char *src, int n);
-
-/*free*/
 void _free_parent(char *buffer, char **commands);
 void _free_double_pointer(char **pointer);
-
-/*tok_tools*/
 unsigned int find_length(char *s);
 char **s_tok(char *str);
-
-/*_built-ins*/
 int check_builtin(char *line, char **command, int *retVal);
 void built_env(char **environ);
-
-/*print_error*/
 void error_printing(char *av, int count, char *command);
 void exec_error(char *av, int count, char *tmp_command);
 void print_string(char *str, int new_line);
