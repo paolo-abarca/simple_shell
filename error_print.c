fc0d5e49bdbfb3ff3e47b3c349fe9ffd0e346a2f
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-04-11 15:37:31 -0500

    adding new version which prints error messages

--- error_print.c
+++ error_print.c
@@ -1,54 +1,41 @@
 #include "main.h"
 
 /**
- * error_printing - Entry point
- *@av: command
- *@count: tokens
- *@command: char string
- * Return: Always 0 (Success)
+ * error_printing - print error messages
+ * @av: is the path folder
+ * @count: tokens
+ * @array: the original command
+ * Return: Always 0
  */
-void error_printing(char *av, int count, char *command)
+void error_printing(char *av, int count, char *array)
 {
 	print_string(av, 1);
 	print_string(": ", 1);
 	print_number(count);
 	print_string(": ", 1);
-	print_string(command, 1);
+	print_string(array, 1);
 }
+
 /**
- * exec_error - Entry point
- *@av: command[0]
- *@count: count tokens
- *@tmp_command: command
+ * print_string - print the error_printing strings
+ * @string: char string
+ * @new_line: is for a line break
  * Return: void
  */
-void exec_error(char *av, int count, char *tmp_command)
-{
-	error_printing(av, count, tmp_command);
-	print_string(": ", 1);
-	perror("");
-	exit(1);
-}
-/**
- * print_string - Entry point
- *@str: char string
- *@new_line: string
- * Return: void
- */
-void print_string(char *str, int new_line)
+void print_string(char *string, int new_line)
 {
 	int i;
 
-	if (str == NULL)
-		str = "(null)";
-	for (i = 0; str[i] != '\0'; i++)
-		write(STDOUT_FILENO, &str[i], 1);
+	if (string == NULL)
+		string = "(null)";
+	for (i = 0; string[i] != '\0'; i++)
+		write(STDOUT_FILENO, &string[i], 1);
 	if (new_line == 0)
 		write(STDOUT_FILENO, "\n", 1);
 }
 /**
  * print_number - Entry point
- *@n: int
+ * @n: int
  * Return: length
  */
 int print_number(int n)
@@ -76,8 +63,8 @@ int print_number(int n)
 }
 /**
  * _write - Entry point
- *@c: character
- * Return: Always 0 (Success)
+ * @c: character
+ * Return: Always 0
  */
 int _write(char c)
 {
