279fc9c8baf14ade5abcca99ad0d23f0ff327825
gerardomp18 <gerardomarinparra18@gmail.com>
2022-04-11 06:43:23 -0500

    Function to free memory of double pointer

---
+++ array_free.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * array_free - release the array
+ *
+ * @array: contains the tokens of the text string
+ */
+
+void array_free(char **array)
+{
+	unsigned int i = 0;
+
+	if (array == NULL)
+		return;
+
+	while (array[i] != NULL)
+	{
+		free(array[i]);
+		++i;
+	}
+
+	if (array[i] == NULL)
+		free(array[i]);
+
+	free(array);
+}
+
+/**
+ * parent_free - release the string
+ * @string: is the string
+ * @array: the tokenized string
+ * Return: void
+ */
+
+void parent_free(char *string, char **array)
+{
+	free(string);
+	array_free(array);
+}
