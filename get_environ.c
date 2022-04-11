504b146285580de07164a5392eaa08960b98e592
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-04-11 15:38:51 -0500

    adding new version that gets the folders from the PATH

--- get_environ.c
+++ get_environ.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * _getenv - gets environment variable.
- * @name: name of variable
- * Return: value
+ * _getenv - get the folders from the PATH variable
+ * @value: is the PATH
+ * Return: all the folders in the PATH
  */
-char *_getenv(const char *name)
+char *_getenv(const char *value)
 {
-	int len = _strlen((char *)name);
+	int len = _strlen((char *)value);
 	char **env = environ;
 
 	while (*env != NULL)
 	{
-		if (_strncmp(*env, (char *)name, len) == 0 && env[0][len] == '=')
+		if (_strncmp(*env, (char *)value, len) == 0 && env[0][len] == '=')
 			return (*env + len + 1);
 		env++;
 	}
